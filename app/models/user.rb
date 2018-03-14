210f08786779c0bec47321de69235646954a6e05
Ryan Riddell <Ryanmriddell@gmail.com>
2018-03-13 18:04:20 -0700

    Fixed Image Uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :players
   has_many :teams, through: :players
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308214834) do
+ActiveRecord::Schema.define(version: 20180313004231) do
 
   create_table "bulletins", force: :cascade do |t|
     t.integer  "team_id"
@@ -73,12 +73,8 @@ ActiveRecord::Schema.define(version: 20180308214834) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
